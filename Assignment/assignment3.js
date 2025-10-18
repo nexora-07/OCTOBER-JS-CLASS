2c4103a247545996ee2e58f0b96ece671fc70107
nexora-07 <akikioriire@gmail.com>
2025-10-18 19:43:30 +0100

    added to ass 3

--- Assignment/assignment3.js
+++ Assignment/assignment3.js
@@ -7,28 +7,45 @@ let firstCase;
 function add() {
   let inputField = document.getElementById("show");
   let userInput = inputField.value.trim();
-
+  let message = document.getElementById("message")
   inputField.value = '';
+  message.textContent = '';
 
   if (userInput === '') {
     alert('Please type something');
     return;
   }
 
-  user = {
-    username: userInput 
-  };
-  nameArr.push(user);
-
   let letter = userInput[0];
   let capitalLetter = letter === letter.toUpperCase() && letter !== letter.toLowerCase();
   let smallLetter = letter === letter.toLowerCase() && letter !== letter.toUpperCase();
 
-  if (capitalLetter) {
-    userInput = user.username;
-  } else if(smallLetter) {
-    userInput = user.username;
-  } 
+  // let list = userInput;
+
+  let found = nameArr.find(item => item.username.toLowerCase() === userInput.toLowerCase());
+
+  if(found) {
+    let existingLetter = found.username[0];
+    let existingIsCapital = existingLetter === existingLetter.toUpperCase() && existingLetter !== existingLetter.toLowerCase();
+
+    if (!existingIsCapital && capitalLetter) {
+      message.textContent = 'Already used';
+      return
+    }
+  }
+
+  user = {
+    username: userInput 
+  };
+  nameArr.push(user);
+  
+//   if (userInput.toLowerCase()) {
+//     if(letter.toUpperCase() && capitalLetter){
+//     } else if(userInput.toLowerCase() && smallLetter) {
+//     message.textContent = 'Alread used';
+//     return;
+//   }
+// }
 
   renderTable();
 
